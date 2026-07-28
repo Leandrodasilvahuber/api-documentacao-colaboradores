36c5769653d132640deb8753587b1718f70e9d7a
Leandro Huber <leandro.huber@hammerconsult.com.br>
2026-07-27 22:43:06 -0300

    feat: cria model collaborator e migration inicial

---
+++ prisma/migrations/20260728013516_create_collaborator_table/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "collaborators" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "deleted_at" TIMESTAMP(3),
+
+    CONSTRAINT "collaborators_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "collaborators_email_key" ON "collaborators"("email");
