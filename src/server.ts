4cd9975dc97de1586b88b99bcd725d6061bc6774
Leandro Huber <leandro.huber@hammerconsult.com.br>
2026-08-02 13:50:33 -0300

    feat: adiciona graceful shutdown ao encerrar o servidor

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,22 @@
 import { app } from "./app";
 import { env } from "./config/env";
 import { logger } from "./shared/logger";
+import { prisma } from "./shared/database/prisma";
 
 const port = env.PORT;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   logger.info(`Servidor rodando em http://localhost:${port}`);
 });
+
+function shutdown(signal: string) {
+  logger.info(`${signal} recebido, encerrando...`);
+  server.close(async () => {
+    await prisma.$disconnect();
+    process.exit(0);
+  });
+  setTimeout(() => process.exit(1), 10000).unref();
+}
+
+process.on("SIGTERM", () => shutdown("SIGTERM"));
+process.on("SIGINT", () => shutdown("SIGINT"));
